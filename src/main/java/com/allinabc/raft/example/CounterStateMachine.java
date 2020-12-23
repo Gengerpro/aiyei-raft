35dfb932c411c0be429ca0f71c2008b01d6442fe
Gengerpro <gaoxinggggg@gmail.com>
2020-12-23 17:43:09 +0800

    .

--- src/main/java/com/allinabc/raft/example/CounterStateMachine.java
+++ src/main/java/com/allinabc/raft/example/CounterStateMachine.java
@@ -2,12 +2,16 @@ package com.allinabc.raft.example;
 
 import com.allinabc.raft.core.RaftIterator;
 import com.allinabc.raft.core.StateMachineAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Fenger
  * @date 2020-12-23 16:57
  */
 public class CounterStateMachine extends StateMachineAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CounterStateMachine.class);
 
     @Override
     public void onApply(RaftIterator iter) {
